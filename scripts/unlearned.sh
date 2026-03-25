b3275c3b0c5e4550a0c248cade2c1bca83c45b3c
hnanhtuan <hnanhtuan.91@gmail.com>
2026-03-26 02:10:30 +1100

    Update depoisoning script to use poison data

--- scripts/unlearned.sh
+++ scripts/unlearned.sh
@@ -2,11 +2,11 @@
 python3 unlearn.py  unlearn_submit/exp/unlearned/smallvgg_cifar10/data_removal/exp1/config.yaml
 
 
-python3 depoisoning.py exp/unlearned/smallvgg_cifar10/depoison/500/config.yaml
-python3 depoisoning.py exp/unlearned/smallvgg_cifar10/depoison/1000/config.yaml
-python3 depoisoning.py exp/unlearned/smallvgg_cifar10/depoison/1500/config.yaml
-python3 depoisoning.py exp/unlearned/smallvgg_cifar10/depoison/2000/config.yaml
-python3 depoisoning.py exp/unlearned/smallvgg_cifar10/depoison/2500/config.yaml
+python3 depoisoning.py exp/unlearned/smallvgg_cifar10/poison/500/config.yaml
+python3 depoisoning.py exp/unlearned/smallvgg_cifar10/poison/1000/config.yaml
+python3 depoisoning.py exp/unlearned/smallvgg_cifar10/poison/1500/config.yaml
+python3 depoisoning.py exp/unlearned/smallvgg_cifar10/poison/2000/config.yaml
+python3 depoisoning.py exp/unlearned/smallvgg_cifar10/poison/2500/config.yaml
 
 
 python3 unlearn_class.py exp/unlearned/resnet18_tinyimagenet/forget_5classes/exp1/config.yaml
